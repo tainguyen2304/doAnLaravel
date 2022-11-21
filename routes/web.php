9055060aa52ed60d67c7373eedadab5311e8b474
Thanh Tai <taint@zen-s.com>
2022-11-21 21:49:39 +0700

    login logout register

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::prefix('admin')->middleware(['auth','isAdmin'])->group(function () {
+    Route::get('dashboard', [App\Http\Controllers\Admin\DashboardController::class, 'index']);
+});
